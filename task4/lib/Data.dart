1fce252b9d29fdb006a0c27bfdeaedc00431b540
Nadaali104 <109938726+Nadaali104@users.noreply.github.com>
2022-09-01 16:17:43 +0200

    Add files via upload

---
+++ task4/lib/Data.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import 'Rooms.dart';
+
+class Data {
+  var counter = 0;
+  List <String> l = [
+   'http://www.gulhaneparkotel.com/images/original/image5e9d9ace2eecb.jpg',
+    'King Room',
+    'A room with a king-sized bed',
+    'http://static1.eskypartners.com/travelguide/twin-room-double-room-jaka-roznica.jpg',
+    'Double Room',
+    'A room assignend to two people',
+    'https://www.frasersproperty.com/content/dam/frasers-hospitality/english/properties/united-kingdom/south-kensington/park-international-hotel-south-kensington/images/gallery-images/rooms/room-type-main-images/quad-room/PIHL_Quad%20Room.jpg',
+    'Family Room',
+    'A room assignend to four people',
+  ];
+}
